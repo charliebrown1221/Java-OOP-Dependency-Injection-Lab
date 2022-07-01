ec24abf5416eed6023a4def52f249f52092bc2e2
charliebrown1221 <chaimbrown1@yahoo.com>
2022-07-01 16:35:41 -0400

    finished lab added input for robot name

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,14 @@ public class Main {
         System.out.println("Hello world!");
 
 
-//        Weapon raygun=new Raygun();
+        Weapon raygun=new Raygun();
 //        Weapon mjolnir=new Mjolnir();
         Weapon bfg =new BFG();
         Attacker robot_one =new Robot(bfg);
-//        Attacker robot_two =new Robot(raygun);
+        Attacker robot_two =new Robot(raygun);
 //        Attacker robot_three =new Robot(mjolnir);
          robot_one.Attack();
-//         robot_two.Attack();
+         robot_two.Attack();
 //         robot_three.Attack();
          Attacker wolf_one =new Werewolf("Remus Lupin",50);
          wolf_one.Attack();
